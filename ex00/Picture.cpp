ac3c0af1c4493c3bb29a9d720ad9874a9524cd47
sherry-du <726845321@qq.com>
2018-07-10 13:45:48 +0800

    ex00

---
+++ ex00/Picture.cpp
@@ -0,0 +1,32 @@
+#include "Picture.h"
+
+Picture::Picture() : data("")
+{
+}
+
+Picture::Picture(const std::string& file)
+{
+  std::ifstream	source(file.c_str());
+  
+  if (!source)
+    data = "ERROR";
+  else
+    getline(source, data, '\0');
+}
+
+Picture::~Picture()
+{
+}
+
+bool Picture::getPictureFromFile(const std::string& file)
+{
+  std::ifstream	source(file.c_str());
+  
+  if (!source)
+  {
+    data = "ERROR";
+    return false;
+  }
+  getline(source, data, '\0');
+  return true;
+}
---
+++ ex00/Toy.cpp
@@ -0,0 +1,36 @@
+#include "Toy.h"
+
+Toy::Toy() : _type(BASIC_TOY), _name("toy"), _data()
+{
+}
+
+Toy::Toy(ToyType type, std::string name, std::string file) : _type(type), _name(name), _data(file)
+{
+}
+
+Toy::~Toy(){}
+
+Toy::ToyType Toy::getType() const
+{
+  return _type;
+}
+
+std::string Toy::getName() const
+{
+  return _name;
+}
+
+void Toy::setName(std::string const& name)
+{
+  _name = name;
+}
+
+bool Toy::setAscii(std::string const& filename)
+{
+  return _data.getPictureFromFile(filename);
+}
+
+std::string Toy::getAscii() const
+{
+  return _data.data;
+}
